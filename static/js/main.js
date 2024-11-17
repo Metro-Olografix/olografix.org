dd6582f75c93fe85e4d3c93a1f512d932959d25c
lopoc <lopoc@users.noreply.github.com>
2024-11-17 23:53:53 +0100

    #8 #7 and minor fixes (#13)
    * font, logo, X
    
    * minor fixes in the header + preview (issue #8) + breadcumb
    
    * footer with social links
    
    * merged and conflict resolved
    
    ---------
    
    Co-authored-by: lopoc <lopoc>

--- static/js/main.js
+++ static/js/main.js
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
       [elements.menuIconOpen, "hidden"],
       [elements.menuText, "hidden"],
       [elements.menuCloseText, "hidden"],
+      [elements.closeButton, "hidden"]
     ].forEach(([element, className]) => {
       if (element) {
         element.classList.toggle(className);
@@ -49,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Event Handlers
   function handleEscapeKey(event) {
     if (event.key === "Escape" && menuState.isOpen) {
+      event.stopPropagation(); // Prevent event bubbling
       toggleMenu(true);
     }
   }
@@ -75,20 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Global event listeners
   document.addEventListener("keydown", handleEscapeKey);
-
-  document
-    .getElementById("mainMenuButton")
-    .addEventListener("click", function () {
-      document.getElementById("mobileMenu").classList.remove("hidden");
-      document.getElementById("mainMenuButton").classList.add("hidden");
-      document.getElementById("closeMenu").classList.remove("hidden");
-    });
-
-  document.getElementById("closeMenu").addEventListener("click", function () {
-    document.getElementById("mobileMenu").classList.add("hidden");
-    document.getElementById("closeMenu").classList.add("hidden");
-    document.getElementById("mainMenuButton").classList.remove("hidden");
-  });
 });
 
 eval(atob("KCgpPT57Y29uc29sZS5sb2coImNpIGhhbm5vIGg0Y2szcmE0dDAhISEhIik7bGV0IGU9ZG9jdW1lbnQucXVlcnlTZWxlY3RvcigiLmh1Z28tdGV4dCIpLHQ9ITEsbj0hMTtlLmFkZEV2ZW50TGlzdGVuZXIoIm1vdXNlZW50ZXIiLCgoKT0+dD0hMCkpLGUuYWRkRXZlbnRMaXN0ZW5lcigibW91c2VsZWF2ZSIsKCgpPT50PSExKSksZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcigia2V5ZG93biIsKG89PnshbiYmdCYmInMiPT09by5rZXkudG9Mb3dlckNhc2UoKSYmKG49ITAsZS5pbm5lckhUTUw9JzxzcGFuIGNsYXNzPSJsZXR0ZXIgcm90YXRlIj5TPC9zcGFuPnVnbycsc2V0VGltZW91dCgoKCk9PntlLmlubmVySFRNTD0nPHNwYW4gY2xhc3M9ImxldHRlciI+SDwvc3Bhbj51Z28nLG49ITF9KSwyZTMpKX0pKX0pKCk7"));
